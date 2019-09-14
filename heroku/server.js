2c277dfbf1511927343b308ab76aaec73fe111c8
Frank <frankwu26@gmail.com>
2019-09-14 10:12:23 -0400

    Migrated existing code base to client side

---
+++ heroku/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const socketServer = require('socket.io')(http);
+
+//Port from environment variable or default - 4001
+const port = process.env.PORT || 4001;
+
+//Setting up a socket with the namespace "connection" for new sockets
+socketServer.on('connection', (socket) => {
+	console.log('A user has connected');
+
+	socket.on('disconnect', () => {
+		console.log('A user has disconnected');
+	})
+
+	socket.on('clientMessage', (msg) => {
+		socket.broadcast.emit('textMessage', msg); //send the message to all clients except the sender
+		console.log(msg);
+	});
+});
+
+http.listen(port, () => console.log(`Listening on port ${port}`));
