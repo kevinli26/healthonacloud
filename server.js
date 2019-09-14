4a63e6fa9646648eceed7e12876a75a3e3060eff
Kevin Li <kevinli1226@gmail.com>
2019-09-14 00:10:50 -0400

    added client messaging interface difference

--- server.js
+++ server.js
@@ -12,13 +12,11 @@ app.get('/', (req,res) => {
 //listens for incoming sockets to the main server
 //will send directly to the sender
 socketServer.on('connection', (socket) => {
-	// console.log('A user has connected');
-	// socket.broadcast.emit('userConnect', "A user has connected");
+	console.log('A user has connected');
 
-	// socket.on('disconnect', () => {
-	// 	console.log('A user has disconnected');
-	// 	socket.broadcast.emit('userDisconnect', "A user has disconnected");
-	// })
+	socket.on('disconnect', () => {
+		console.log('A user has disconnected');
+	})
 
 	socket.on('clientMessage', (msg) => {
 		socket.broadcast.emit('textMessage', msg); //send the message to all clients except the sender
