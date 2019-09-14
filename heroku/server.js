f4c2b7abb1dbfe9930b8b608f6c4491b6b9f296d
Kevin Li <kevinli1226@gmail.com>
2019-09-14 15:47:39 -0400

    working on heroku instance

--- heroku/server.js
+++ heroku/server.js
@@ -18,18 +18,18 @@ app.get("/", (req,res) => {
 	res.send("Root Endpoint. Only use as a RESTful api.")
 })
 
-app.post("/sendEmail", (req,res) => {
-	const sgMail = require('@sendgrid/mail');
-	sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-	const msg = {
-	  to: 'test@example.com',
-	  from: 'test@example.com',
-	  subject: 'Sending with Twilio SendGrid is Fun',
-	  text: 'and easy to do anywhere, even with Node.js',
-	  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-	};
-	sgMail.send(msg);
-})
+// app.post("/sendEmail", (req,res) => {
+// 	const sgMail = require('@sendgrid/mail');
+// 	sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// 	const msg = {
+// 	  to: 'kevinli1226@example.com',
+// 	  from: 'test@example.com',
+// 	  subject: 'Sending with Twilio SendGrid is Fun',
+// 	  text: 'and easy to do anywhere, even with Node.js',
+// 	  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+// 	};
+// 	sgMail.send(msg);
+// })
 
 app.post("/endSession", (req,res) => {
 	//call azure API to summarize
