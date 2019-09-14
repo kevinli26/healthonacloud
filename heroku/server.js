64ce4e20d322aea07ac241a98db245d9c3f662c1
Kevin Li <kevinli1226@gmail.com>
2019-09-14 13:10:03 -0400

    updated end points

--- heroku/server.js
+++ heroku/server.js
@@ -6,6 +6,9 @@ const socketServer = require('socket.io')(http);
 //Port from environment variable or default - 4001
 const port = process.env.PORT || 4001;
 
+app.get("/", (req,res) => {
+	res.send("Root Endpoint. Only use as a RESTful api.")
+})
 //Setting up a socket with the namespace "connection" for new sockets
 socketServer.on('connection', (socket) => {
 	console.log('A user has connected');
